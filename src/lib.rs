18a4a30c33c49477c49f1fac8384dfb08f11d5af
vibhushit <vibhushit10@gmail.com>
2024-09-01 00:24:40 +0530

    add: robustness

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,17 @@
 //! lib.rs
 
-use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use std::net::TcpListener;
+
 use actix_web::dev::Server;
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
 async fn health_check() -> impl Responder {
     HttpResponse::Ok().finish()
 }
 
-pub  fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-        })
-        .bind("127.0.0.1:8000")?
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .listen(listener)?
         .run();
 
     Ok(server)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,22 +1,23 @@
 //! tests/health_check.rs
 
-
 // `tokio::test` is the testing equivalent of `tokio::main`.
 //It also spares you from having to specify the `#[test]` attribute.
+
+use std::net::TcpListener;
 
 //you can inspecct what code gets generated using
 //`cargo expand --test helath_check` (<- name of the test file)
 #[tokio::test]
 async fn health_check_works() {
     //Arrange
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in `reqwest`
     //to perform HTTP requests against our application.
-    let client =reqwest::Client::new();
+    let client = reqwest::Client::new();
 
     //Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execcute request.");
@@ -26,7 +27,10 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-fn spawn_app()  {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("failed to bind address");
     let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
 }
