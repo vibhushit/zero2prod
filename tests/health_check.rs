5f378ee715387c4a34735636eeccd6f2e388fd0a
vibhushit <vibhushit10@gmail.com>
2024-09-01 21:34:12 +0530

    add:test cases for subscription endpoint

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -34,3 +34,55 @@ fn spawn_app() -> String {
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    //Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    //Act
+    let body = "name=vibhushit%20tyagi&email=vibhushit%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions",&app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    //Assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    //Arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=vibhushit%20tyagi", "missing the email"),
+        ("email=vibhushit%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    
+    for (invalid_body, error_message) in test_cases {
+        //Act
+        let response = client
+            .post(&format!("{}/subsccription",&app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            400, 
+            response.status().as_u16(),
+            //additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.", 
+            error_message,
+        );
+    }
+}
