21a98055da16a853c881f2885f5a02533d8501dc
vibhushit <vibhushit10@gmail.com>
2024-09-03 22:31:40 +0530

    refactor: test cases

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,6 +5,14 @@
 
 use std::net::TcpListener;
 
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("failed to bind address");
+    let _ = tokio::spawn(server);
+    format!("http://127.0.0.1:{}", port)
+}
+
 //you can inspecct what code gets generated using
 //`cargo expand --test helath_check` (<- name of the test file)
 #[tokio::test]
@@ -25,14 +33,6 @@ async fn health_check_works() {
     //Assert
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
-}
-
-fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("failed to bind address");
-    let _ = tokio::spawn(server);
-    format!("http://127.0.0.1:{}", port)
 }
 
 #[tokio::test]
