46202724c7f79ead429de2e4e82eab0d248d0c77
vibhushit <vibhushit10@gmail.com>
2024-09-13 00:35:19 +0530

    add: tracing instrument

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,34 @@
 
 use std::net::TcpListener;
 
-// use sqlx::{Connection, PgConnection};
-use env_logger::Env;
 use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
+use tracing::subscriber::{self, set_global_default};
+use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
+use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
+use tracing_log::LogTracer;
+
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+    // env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+   LogTracer::init().expect("Failed to set logger");
+   
+    let env_filter = EnvFilter::try_from_default_env()
+        .unwrap_or_else(|_| EnvFilter::new("info"));
+
+    let formatting_layer = BunyanFormattingLayer::new(
+        "zero2prod".into(),
+        std::io::stdout
+    );
+
+    let subscriber = Registry::default()
+        .with(env_filter)
+        .with(JsonStorageLayer)
+        .with(formatting_layer);
+
+    set_global_default(subscriber).expect("failed to set subscriber");
 
     //Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -4,6 +4,7 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use tracing::Instrument;
 
 #[allow(dead_code)]
 #[derive(serde::Deserialize)]
@@ -14,16 +15,17 @@ pub struct FormData {
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
     let request_id = Uuid::new_v4();
-    log::info!(
-        "request_id {} - Adding '{}' '{}' as a new subscriber.",
-        request_id,
-        form.email,
-        form.name
+    let request_span = tracing::info_span!(
+        "Adding  a new subscriber.",
+        %request_id,
+        subscriber_email = %form.email,
+        subscriber_name = %form.name
     );
-    log::info!(
+    let _request_span_guard = request_span.enter();
+    
+    let query_span = tracing::info_span!(
         "
-    request_id {} - Saving new subscriber details in the database",
-        request_id
+    Saving new subscriber details in the database"
     );
     match sqlx::query!(
         r#"
@@ -38,24 +40,20 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     //we use `get_ref` to get an immutable reference to the PgConnection
     //wrapped by `web::Data`.
     .execute(pool.as_ref())
+    .instrument(query_span)
     .await
     {
         Ok(_) => {
-            log::info!(
-                "
-                request_id {} - New subscriber details ahve been saved",
-                request_id
-            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!(
+            tracing::error!(
                 "
-                request_id {} - Failed to execute query: {:?}",
-                request_id,
+                Failed to execute query: {:?}",
                 e
             );
             HttpResponse::InternalServerError().finish()
         }
     }
 }
+
